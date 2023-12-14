d0b064617475e4ba800aef8d7185e4080a3f58d0
bilalbg <bilal.baig316@gmail.com>
2023-12-14 14:30:12 -0500

    Managing State and Life Cycle

---
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Bilal Baig on 2023-12-14.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingHeaderView()
+}
